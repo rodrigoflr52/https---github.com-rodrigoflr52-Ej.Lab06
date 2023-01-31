ff4b72ec37ca305883d089aa04f5fb0d11631825
rodrigoflr52 <rodrigoflores52@hotmail.com>
2023-01-30 22:51:59 -0600

    cambios

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 (function () {
 
-
+;
   function giphySearch(keyword) {
     return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${GIPHY_KEY}&limit=5`)
       .then(response => response.json());
